494f69a3eb4600cd35ce21778d734c030ba038b7
Ivan Vitkvoskiy <cloudfly97@icloud.com>
2025-10-14 14:33:24 +0300

    fixed)

---
+++ lab1/plain.js
@@ -0,0 +1,59 @@
+
+class User {
+  constructor(id, name, login, password, bonus_card = false) {
+    this.id = id;                // PK
+    this.name = name;
+    this.login = login;
+    this.password = password;
+    this.bonus_card = bonus_card;
+  }
+}
+
+class Room {
+  constructor(room_id, type, free_seats, with_bed = false) {
+    this.room_id = room_id;      // PK
+    this.type = type;
+    this.free_seats = free_seats;
+    this.with_bed = with_bed;
+  }
+}
+
+class Categories {
+  constructor(category, name, type) {
+    this.category = category;    // PK
+    this.name = name;
+    this.type = type;
+  }
+}
+
+class Movie {
+  constructor(id, category, name, rate, oscar = false, release_date) {
+    this.id = id;                // PK
+    this.category = category;    // FK → categories.category
+    this.name = name;
+    this.rate = rate;
+    this.oscar = oscar;
+    this.release_date = release_date;
+  }
+}
+
+class MoviesList {
+  constructor(movie_id, category, name) {
+    this.movie_id = movie_id;    // PK, FK → movie.id
+    this.category = category;    // FK → categories.category
+    this.name = name;
+  }
+}
+
+class Order {
+  constructor(session_id, date, ticket_id, movie_id, category, payment, user_id, room_id) {
+    this.session_id = session_id;  // PK
+    this.date = date;
+    this.ticket_id = ticket_id;
+    this.movie_id = movie_id;      // FK → movie.id
+    this.category = category;      // FK → categories.category
+    this.payment = payment;
+    this.user_id = user_id;        // FK → user.id
+    this.room_id = room_id;        // FK → room.room_id
+  }
+}
